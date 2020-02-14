ab99f55b0c1ebb0e04edc21c80ece2eed50d1eae
Filip Dimitrovski <filip@ifkreativa.com>
2020-02-14 14:06:13 +0100

    First commit

---
+++ www/js/ihr.js
@@ -0,0 +1,268 @@
+﻿var domain = "https://ihr.ifkreativa.com";
+
+function DownloadAndUpdateHit(guid, link)
+{
+    window.open(link, '_system');
+    var uuid = device.uuid;
+    $.ajax({
+        //url: domain + "/api/updateHits",
+        //data: JSON.stringify({ guid: guid, uuid: device.uuid}),
+        url: domain + "/api/updateHits?guid=" + guid + "&uuid=" + uuid,
+        type: "POST",
+        dataType: "jsonp",
+        contentType: "application/json; charset=utf-8",
+        success: function (data) {
+            if (data == "success")
+                UpdateNumber(guid);
+            //if (data["status"] == "fail")
+            //    alert("Превземањето не е евидентирано.");
+            //else if (data["status"] == "success")
+            //    alert("Презвемањето е евидентирано.");
+        },
+        error: function (e) {
+            alert(e.responseText);
+        }
+    })
+}
+
+function getBrief(part) {
+    $(".loading-wrap").removeClass("hidden");
+    var take = 5;
+    var skipCount = getSkipCount();
+    var apiAction = "";
+    if (part == "sudski")
+        apiAction = "getBrief";
+    else if (part == "antikorupciska")
+        apiAction = "getBriefAntikorupciska";
+    else apiAction = "getBriefAntidiskriminatorska";
+
+    $.ajax({
+        url: domain + "/api/" + apiAction + "?lang=mk&skip=" + skipCount + "&take=" + take,
+        type: "GET",
+        dataType: "jsonp",
+        success: function (data) {
+            if (data["status"] == "OK") {
+                var html = "";
+                $.each(data["messages"], function (index, el) {
+                    html += "<div class='page-blog-list' onclick='OpenCloseItem(this)'>"
+                        //+ "<div class='page-blog-tags'><i class='ion-calendar date-icon'></i> Објавено на: " + el.publishDate + "</div><h4 class='page-blog-title'>" + el.title + "</h4>"
+                        + "<div class='page-blog-tags'>" + el.publishDate + "</div><h4 class='page-blog-title'>" + el.title + "</h4>"
+                        + "<div class='page-blog-content hidden'><p>" + el.description + "</p>"
+                        + "</div><div class='clear'></div>"
+                            + "</div><div class='decoration'></div>";
+                });
+                //html += "<div>Token: " + localStorage.token + "</div>"
+                $(".blog-posts").append(html);
+                $(".loading-wrap").addClass("hidden");
+                if (!data["hasMore"])
+                    $(".btn-get-events").hide();
+            }
+            else
+            {
+                var html = "";
+                $(".loading-wrap").addClass("hidden");
+                html += "Во моментов нема бриф информации за приказ";
+                $(".blog-posts").append(html);
+                $(".btn-get-events").hide();
+            }
+        },
+        error: function (error) {
+            //navigator.notification.alert(
+            //    error,  
+            //    onNoNetworkConfirm,
+            //    'Порака',
+            //    'ОК' 
+            //);
+            showNoNetwork();
+        }
+    
+    })
+}
+
+function getEvents(part) {
+    $(".loading-wrap").removeClass("hidden");
+    var take = 5;
+    var skipCount = getSkipCount();
+
+    var apiAction = "";
+    if (part == "sudski")
+        apiAction = "getMessages";
+    else if (part == "antikorupciska")
+        apiAction = "getMessagesAntikorupciska";
+    else apiAction = "getMessagesAntidiskriminatorska";
+
+    $.ajax({
+        url: domain + "/api/" + apiAction + "?lang=mk&skip=" + skipCount + "&take=" + take,
+        type: "GET",
+        dataType: "jsonp",
+        success: function (data) {
+            if (data["status"] == "OK") {
+                var html = "";
+                $.each(data["messages"], function (index, el) {
+                    html += "<div class='page-blog-list' onclick='OpenCloseItem(this)'>"
+                        + "<div class='page-blog-tags'>" + el.publishDate + "</div><h4 class='page-blog-title'>" + el.title + "</h4>"
+                        + "<div class='page-blog-content hidden'><p>" + el.description + "</p>"
+                        + "</div><div class='clear'></div>"
+                            + "</div><div class='decoration'></div>";
+
+                });
+                $(".blog-posts").append(html);
+                $(".loading-wrap").addClass("hidden");
+                if (!data["hasMore"])
+                    $(".btn-get-events").hide();
+            }
+            else {
+                var html = "";
+                $(".loading-wrap").addClass("hidden");
+                html += "Во моментов нема информации за приказ";
+                $(".blog-posts").append(html);
+                $(".btn-get-events").hide();
+            }
+
+        },
+        error: function (error)
+        {
+            showNoNetwork();
+        }
+
+    })
+}
+
+function getDocuments(_type, part) {
+    $(".loading-wrap").removeClass("hidden");
+    var take = 5;
+    var skipCount = getSkipCount();
+
+    var apiAction = "";
+    if (part == "sudski")
+        apiAction = _type == "povrzani-dokumenti" ? "getDocuments" : "getReports";
+    else if (part == "antikorupciska")
+        apiAction = _type == "povrzani-dokumenti" ? "getDocumentsAntikorupciska" : "getReportsAntikorupciska";
+    else apiAction = _type == "povrzani-dokumenti" ? "getDocumentsAntidiskriminatorska" : "getReportsAntidiskriminatorska";
+   
+    $.ajax({
+        url: domain + "/api/" + apiAction + "?skip=" + skipCount + "&take=" + take,
+        type: "GET",
+        dataType: "jsonp",
+        success: function (data) {
+            if (data["status"] == "OK") {
+                var html = "";
+                $.each(data["documents"], function (index, el) {
+                    html += '<div class="page-blog-list">' +
+                                '<div class="page-blog-tags">' + el.publishDate + '</div>' +
+                                '<h4 class="page-blog-title">' + el.name + '</h4>' +
+                                '<div class="page-blog-list-by">' +
+                                    '<a class="button btn-download" onClick="DownloadAndUpdateHit(\'' + el.guid + '\',\'' + domain + el.link + '\')"><i class="ion-ios-download date-icon"></i> Превземи</a>' +
+                                '</div>' +
+                                '<div class="clear"></div>' +
+                                //'<div class="">вкупно превземања: <span class="doc-' + el.guid + '">' + el.hits + '</span></div>' +
+                            '</div>' +
+                            '<div class="decoration"></div>';
+
+                });
+                $(".loading-wrap").addClass("hidden");
+                $(".blog-posts").append(html);
+
+                if (!data["HasMore"])
+                    $(".btn-get-events").hide();
+            }
+            else {
+                var html = "";
+                $(".loading-wrap").addClass("hidden");
+                html += "Во моментов нема информации за приказ";
+                $(".blog-posts").append(html);
+                $(".btn-get-events").hide();
+            }
+        },
+        error: function (error) {
+            showNoNetwork();
+        }
+    });
+    
+}
+
+function getSkipCount() {
+    return $(".page-blog-list").length > 0 ? $(".page-blog-list").length : 0;
+}
+
+function getContent(location) {
+    if (location.includes("kvartalni.html")) {
+        localStorage.page = "kvartalni-sudski";
+        getDocuments('kvartalni-izvestai','sudski');
+    }
+    else if (location.includes("kvartalni-antidiskriminatorska.html")) {
+        localStorage.page = "kvartalni-antisdiskriminatorska";
+        getDocuments('kvartalni-izvestai','antidiskriminatorska');
+    }
+    else if (location.includes("kvartalni-antikorupciska.html")) {
+        localStorage.page = "kvartalni-antikorupciska";
+        getDocuments('kvartalni-izvestai', 'antikorupciska');
+    }
+    else if (location.includes("dokumenti.html")) {
+        localStorage.page = "povrzani-dokumenti-sudski";
+        getDocuments('povrzani-dokumenti','sudski');
+    }
+    else if (location.includes("dokumenti-antikorupciska.html")) {
+        localStorage.page = "povrzani-dokumenti-antikorupciska";
+        getDocuments('povrzani-dokumenti', "antikorupciska");
+    }
+    else if (location.includes("dokumenti-antidiskriminatorska.html")) {
+        localStorage.page = "povrzani-dokumenti-antidsikriminatorska";
+        getDocuments('povrzani-dokumenti', "antidsikriminatorska");
+    }
+    else if (location.includes("events.html")) {
+        //localStorage.page = "homepage";
+        localStorage.page = "events-sudski";
+        getEvents("sudski");
+    }
+    else if (location.includes("events-antikorupciska.html")) {
+        //localStorage.page = "homepage";
+        localStorage.page = "events-antikorupciska";
+        getEvents("antikorupciska");
+    }
+    else if (location.includes("events-antidiskriminatorska.html")) {
+        //localStorage.page = "homepage";
+        localStorage.page = "events-antidiskriminatorska";
+        getEvents("antidiskriminatorska");
+    }
+    else if (location.includes("index-sudski.html"))
+    {
+        getBrief("sudski");
+        localStorage.page = "brief-sudski";
+    }
+    else if (location.includes("index-antidiskriminatorska.html")) {
+        getBrief("antidiskriminatorska");
+        localStorage.page = "brief-antidiskriminatorska";
+    }
+    else{
+        getBrief("antikorupciska");
+        localStorage.page = "brief-antikorupciska";
+    }
+}
+
+function showNoNetwork() {
+    navigator.notification.alert(
+        'Во моментов немате активна интернет конекција. Вклучете интернет и стиснете ок.',  // message
+        onNoNetworkConfirm,              // callback to invoke with index of button pressed
+        'Порака',            // title
+        'ОК'          // buttonLabels
+    );
+}
+
+function onNoNetworkConfirm() {
+    getContent(window.location.href);
+}
+
+function UpdateNumber(guid)
+{
+    var number = parseInt($(".doc-" + guid).text());
+    number = number + 1;
+    $(".doc-" + guid).text(number);
+}
+
+function OpenCloseItem(el)
+{
+    if ($(el).find(".page-blog-content").hasClass("hidden"))
+        $(el).find(".page-blog-content").removeClass("hidden");
+    else $(el).find(".page-blog-content").addClass("hidden")
+}
---
+++ www/js/index.js
@@ -0,0 +1,254 @@
+﻿
+function onPushwooshInitialized(pushNotification) {
+
+    //if you need push token at a later time you can always get it from Pushwoosh plugin
+    pushNotification.getPushToken(
+        function (token) {
+            console.info('push token: ' + token);
+        }
+    );
+
+    //and HWID if you want to communicate with Pushwoosh API
+    pushNotification.getPushwooshHWID(
+        function (token) {
+            console.info('Pushwoosh HWID: ' + token);
+        }
+    );
+
+    //settings tags
+    pushNotification.setTags({
+        tagName: "tagValue",
+        intTagName: 10
+    },
+        function (status) {
+            console.info('setTags success: ' + JSON.stringify(status));
+        },
+        function (status) {
+            console.warn('setTags failed');
+        }
+    );
+
+    pushNotification.getTags(
+        function (status) {
+            console.info('getTags success: ' + JSON.stringify(status));
+        },
+        function (status) {
+            console.warn('getTags failed');
+        }
+    );
+
+    //start geo tracking.
+    //pushNotification.startLocationTracking();
+}
+
+function getNotificationTitle(message)
+{
+    if (message.includes("[0]") || message.includes("[1]") || message.includes("[2]") || message.includes("[3]"))
+        return "Судски Совет под лупа";
+    else if (message.includes("[4]") || message.includes("[5]") || message.includes("[6]") || message.includes("[7]"))
+        return "Анти-корупциска под лупа";
+    else if (message.includes("[8]") || message.includes("[9]") || message.includes("[10]") || message.includes("[11]"))
+        return "Анти-дискриминаторска под лупа";
+    else return "Институции под лупа";
+}
+
+function RedirectToSection(message)
+{
+    if (message.includes("[0]"))
+        return "index-sudski.html?link=0";
+    else if (message.includes("[1]"))
+        return "events.html?link=1";
+    else if (message.includes("[2]"))
+        return "kvartalni.html?link=2";
+    else if (message.includes("[3]"))
+        return "dokumenti.html?link=3";
+    if (message.includes("[4]"))
+        return "index-antikorupciska.html?link=4";
+    else if (message.includes("[5]"))
+        return "events-antikorupciska.html?link=5";
+    else if (message.includes("[6]"))
+        return "kvartalni-antikorupciska.html?link=6";
+    else if (message.includes("[7]"))
+        return "dokumenti-antikorupciska.html?link=7";
+    if (message.includes("[8]"))
+        return "index-antidiskriminatorska.html?link=8";
+    else if (message.includes("[9]"))
+        return "events-antidiskriminatorska.html?link=9";
+    else if (message.includes("[10]"))
+        return "kvartalni-antidiskriminatorska.html?link=10";
+    else if (message.includes("[11]"))
+        return "dokumenti-antidiskriminatorska.html?link=11";
+    else return "index.html";
+}
+
+function initPushwoosh() {
+    var pushNotification = cordova.require("pushwoosh-cordova-plugin.PushNotification");
+
+    //set push notifications handler
+    document.addEventListener('push-notification',
+        function (event) {
+            var message = event.notification.message;
+            var pomoshna = message;
+            var filteredMessage = pomoshna.replace("[1]", "").replace("[2]", "").replace("[3]", "").replace("[4]", "").replace("[5]", "").replace("[6]", "").replace("[7]", "").replace("[8]", "").replace("[9]", "").replace("[10]", "").replace("[11]", "").replace("[0]", "");
+            var userData = event.notification.userdata;
+
+            navigator.notification.alert(filteredMessage, function () {
+                lokacija = RedirectToSection(message);
+                window.location.href = RedirectToSection(message);
+            }, getNotificationTitle(message), 'ОК');
+
+            //navigator.notification.alert(JSON.stringify(event.notification), function () {
+            //}, 'Судски совет под лупа', 'ОК');
+
+            //document.getElementById("pushMessage").innerHTML = message + "<p>";
+            //document.getElementById("pushData").innerHTML = JSON.stringify(event.notification) + "<p>";
+
+            //dump custom data to the console if it exists
+            if (typeof (userData) != "undefined") {
+                //alert(JSON.stringify(userData));
+                console.warn('user data: ' + JSON.stringify(userData));
+            }
+        }
+    );
+
+    pushNotification.onDeviceReady({
+        appid: "1ED35-90D1C",
+        projectid: "622748095704",
+        serviceName: ""
+    });
+
+    //register for push notifications
+    pushNotification.registerDevice(
+        function (status) {
+            //alert("Success: " + status.pushToken);
+            //document.getElementById("pushToken").innerHTML = status.pushToken + "<p>";
+            onPushwooshInitialized(pushNotification);
+            //localStorage.token = status.pushToken;
+            //navigator.notification.alert("Register Device fired. Token: " + status.pushToken, function () {
+
+            //}, 'Судски совет под лупа', 'ОК');
+            //localStorage.token = status.pushToken;
+        },
+        function (status) {
+            //alert("failed to register: " + status);
+            console.warn(JSON.stringify(['failed to register ', status]));
+        }
+    );
+}
+
+function Initalize() {
+    bindEvents();
+}
+
+function bindEvents() {
+    document.addEventListener('deviceready', onDeviceReady, false);
+}
+
+function onDeviceReady() {
+    receivedEvent('deviceready');
+    initPushwoosh();
+}
+
+function receivedEvent(id) {
+
+    console.log('Received Event: ' + id);
+    localStorage.platform = device.platform;
+    //localStorage.token = pushNotification.getPushToken();
+    //navigator.notification.alert("Token:" + localStorage.getItem("pushwooshToken"), function () {
+    //}, 'Судски совет под лупа', 'ОК');
+
+    //getContent(window.location.href);
+    document.addEventListener("pause", onPause, false);
+    document.addEventListener("backbutton", onBackKeyDown, false);
+    document.addEventListener("menubutton", onMenuKeyDown, false);
+    document.addEventListener("searchbutton", onSearchKeyDown, false);
+    document.addEventListener("offline", onOffline, false);
+    document.addEventListener("online", onOnline, false);
+    document.addEventListener("resume", onResume, false);
+}
+
+
+function onPause() {
+    // Handle the back button
+}
+
+function CloseApp()
+{
+    if (confirm("Дали сте сигурни дека сакате да ја затворите апликацијата?")) {
+        navigator.app.exitApp();
+    }
+}
+
+function onBackKeyDown(e) {
+    var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+    if (localStorage.page) {
+        if (localStorage.page == "homepage")
+        {
+            if (confirm("Дали сте сигурни дека сакате да ја затворите апликацијата?")) {
+                  navigator.app.exitApp();
+            }
+        }
+        else
+        {
+            window.location.href = "index.html";
+            localStorage.page = "homepage";
+        }
+        //if (localStorage.page == "homepage") {
+        //    if (userAgent.match(/iPad/i) || userAgent.match(/iPhone/i) || userAgent.match(/iPod/i)) {
+
+        //    }
+        //    else if (confirm("Дали сте сигурни дека сакате да ја затворите апликацијата?")) {
+        //        navigator.app.exitApp();
+        //    }
+        //}
+        //else {
+        //    if (userAgent.match(/iPad/i) || userAgent.match(/iPhone/i) || userAgent.match(/iPod/i)) {
+        //        // IOS DEVICE
+        //        history.go(-2);
+        //    } else if (userAgent.match(/Android/i)) {
+        //        // ANDROID DEVICE
+        //        history.go(-1);
+        //        navigator.app.backHistory();
+        //    } else {
+        //        // EVERY OTHER DEVICE
+        //        history.go(-1);
+        //        history.go(-1);
+        //    }
+        //}
+    }
+    else {
+
+    }
+}
+
+function onMenuKeyDown() {
+    //Handle the menu key down button
+}
+
+function onSearchKeyDown() {
+    // Handle the back button
+}
+
+function onOffline() {
+    //alert("You are offline now");
+    // Handle the offline event
+}
+
+function onOnline() {
+    // Handle the online event
+    //alert("You are online now");
+}
+
+function onResume() {
+    // Handle resume event
+    //$("#preloader").addClass("hide");
+}
+
+
+
+
+
+
+
+
+
